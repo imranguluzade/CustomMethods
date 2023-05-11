1329845add97f429817ac798e4b346a4715a641c
imranguluzade <imranguliyev211@gmail.com>
2023-05-11 16:34:28 +0400

    script injected

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // * Max Number uzerinden manipulyasiya - Callback vasitesile
+
 const numbers = [1, 2, 3, 4, 5, 6, 7]; //example array
 function getMaxNum(numbers, callback) {
   const maxNumber = Math.max(...numbers);
@@ -11,3 +12,34 @@ const summary = getMaxNum(numbers, function (maxNum) {
 console.log(summary);
 
 // * Custom forEach Method creating with Callback
+
+const arr = [1, 2, 3, 4, 5, 6, 7]; //example array
+function customForEach(arr, callback) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callback(arr[i]));
+  }
+  return result;
+}
+const result = customForEach(arr, function (item) {
+  return item;
+});
+console.log(result);
+
+// * Custom Find Method creating with Callback
+
+const array = [1, 2, 3, 4, 5, 6, 7]; //example array
+function customFind(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    const isTrue = callback(array[i]);
+    if (isTrue === true) {
+      return arr[i];
+    }
+  }
+}
+const res = customFind(array, function (el) {
+  return el > 3;
+});
+console.log(res);
+
+
